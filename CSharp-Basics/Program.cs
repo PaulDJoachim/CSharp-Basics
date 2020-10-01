8ba5053682c014f2f56bbc0f503ca2fae6c35cfc
Paul Joachim <63604194+PaulDJoachim@users.noreply.github.com>
2020-09-30 19:25:23 -0500

    changed starting ints

--- CSharp-Basics/Program.cs
+++ CSharp-Basics/Program.cs
@@ -6,6 +6,12 @@ namespace CSharp_Basics
   {
     static void Main(string[] args)
     {
+
+      // Settings
+      Console.BufferHeight = 400;
+      Console.BufferWidth = 400;
+      Console.SetWindowSize(150,50);
+
       //Ints:
       int Char_Health_Full = 100;
       int Char_Health_Current = 100;
@@ -23,37 +29,8 @@ namespace CSharp_Basics
       //Program:
       do
       {
-        Console.Clear();  
-        int Spawn_Point_Generator_X = Random.Next (0, 230);
-        int Spawn_Point_Generator_Y = Random.Next (0, 77);
-        int Spawn_Point_Height = Random.Next(5, 8);
-        int Spawn_Point_Width = Random.Next(7, 10);
-
-        for (int x = 0; x <= Spawn_Point_Height; x++)
-        {
-          for (int y = 0; y <= Spawn_Point_Width; y++)
-          {
-            if(y == 0 || y == Spawn_Point_Height-1)
-            {
-              Draw_Game_Map[Spawn_Point_Generator_X+x, Spawn_Point_Generator_Y+y] = "_";
-            }
-            else
-            {
-              if(x == 0 || x == Spawn_Point_Width)
-              {
-                Draw_Game_Map[Spawn_Point_Generator_X + x, Spawn_Point_Generator_Y + y] = "|";
-              }
-            }
-          }
-        }
-        for (int x_Draw = 0; x_Draw <= 229; x_Draw++)
-        {
-          for (int y_Draw = 0; y_Draw <= 76; y_Draw++)
-          {
-            Console.Write(Draw_Game_Map[x_Draw, y_Draw]);
-          }
-        }
-        Console.ReadLine();
+        //Console.Clear();
+        Console.Write('t');
       } while (Gameover == 0);
 
     }
